50536cb3f53a4e16edcdb0f514e508b02358baef
Humayan Kabir <zyxwv.hk@gmail.com>
2024-07-17 12:53:40 +0600

    setup spring project with jdbc

---
+++ src/main/java/com/kabir/project/spring_jpa_hibernate/SpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.kabir.project.spring_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateApplication.class, args);
+	}
+
+}
